ba2741f5c1473716b57bd757a4e4f95287e3edbb
Natali <114168847+Natali9108@users.noreply.github.com>
2023-03-21 23:27:47 +0200

    add part of work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 import './css/styles.css';
 
+const refs = {
+  countryInfo: document.querySelector('.country-info'),
+  input: document.querySelector('#search-box'),
+};
 const DEBOUNCE_DELAY = 300;
+
+refs.input.addEventListener('input', handelSeachCountryInput);
+
+function handelSeachCountryInput(evt) {
+  const searchCountry = evt.target.value;
+  fetchCountries(searchCountry).then(renderCountryCard);
+}
+
+function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages
+    `
+  ).then(response => {
+    return response.json();
+  });
+}
+
+function createCountryCard(country) {
+  return country.map(
+    ({ name, capital, population, flags, languages }) =>
+      `<div class="country-wrapper">
+          <img class="country-flag" src="${flags.svg}" alt="${
+        flags.alt
+      }" width='40' height='30'/>
+        <h1 class="country-title">${name.official}</h1></div>
+        <ul class="country-list">
+        <li class="country-item">
+        <p class="country-property">Capital:</p>
+         <span class="country-property-value">${capital}</span></li>
+      <li class="country-item">
+      <p class="country-property">Population:</p>
+       <span class="country-property-value">${population}</span> </li>
+      <li class="country-item">
+      <p class="country-property">Language:</p> 
+      <span class="country-property-value">${Object.values(
+        languages
+      )}</span> </li></ul>`
+  );
+}
+
+function renderCountryCard(countryName) {
+  refs.countryInfo.innerHTML = createCountryCard(countryName);
+}
